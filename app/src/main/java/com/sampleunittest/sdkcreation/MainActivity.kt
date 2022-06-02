07160b86373a4472b2f7de24341ed205f9a55079
sandeepkumar9248 <sandeep.k@peopelinkvc.com>
2022-06-02 17:02:11 +0530

    RetrofitExample

--- app/src/main/java/com/sampleunittest/sdkcreation/MainActivity.kt
+++ app/src/main/java/com/sampleunittest/sdkcreation/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sampleunittest.sdkcreation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.sampleunittest.mylibrary.ToasterMessage
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+
+            Log.d("hello", "onCreate: " + ToasterMessage().getDataFromLibrary(this@MainActivity, "helo"))
+        }
     }
 }
--- mylibrary/src/main/java/com/sampleunittest/mylibrary/ToasterMessage.kt
+++ mylibrary/src/main/java/com/sampleunittest/mylibrary/ToasterMessage.kt
@@ -1,8 +1,9 @@
 package com.sampleunittest.mylibrary
 
 import android.content.Context
-import android.graphics.Movie
+import android.util.Log
 import android.widget.Toast
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,29 +11,44 @@ import retrofit2.Response
 
 class ToasterMessage {
 
-    fun getDataFromLibrary(c: Context?, message: String?): UsersResponse {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
-        var usersResponse: UsersResponse? = null
-        val apiInterface = ApiInterface.create().getMovies()
+    suspend fun getDataFromLibrary(c: Context?, message: String?): List<DataItem?>? {
+//        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        var usersResponse: List<DataItem?>? = arrayListOf()
+
+        val value = GlobalScope.async {
+            val apiInterface = ApiInterface.create().getMovies()
+            val response: Response<UsersResponse> = apiInterface.execute()
+            usersResponse = response.body().data
+            Log.d("Error", "onFailure: ${usersResponse}")
+
+        }
+        println("value =  ${value.await()} thread running on [${Thread.currentThread().name}]")
+
 
         //apiInterface.enqueue( Callback<List<Movie>>())
-        apiInterface.enqueue(object : Callback<UsersResponse> {
-            override fun onResponse(
-                call: Call<UsersResponse>?,
-                response: Response<UsersResponse>?
-            ) {
 
-                if (response?.body() != null) {
-                    usersResponse = response.body()
-                }
-            }
 
-            override fun onFailure(call: Call<UsersResponse>?, t: Throwable?) {
+//        val apiResponse: UsersResponse = response.body()
+//        apiInterface.enqueue(object : Callback<UsersResponse> {
+//            override fun onResponse(
+//                call: Call<UsersResponse>?,
+//                response: Response<UsersResponse>?
+//            ) {
+//                Log.d("Error", "onFailure: ${response?.message()}")
+//
+//                if (response?.body() != null) {
+//                    usersResponse = response.body().data
+//                    Log.d("Error", "onFailure: ${usersResponse}")
+//
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<UsersResponse>?, t: Throwable?) {
+//                Log.d("Error", "onFailure: ${t!!.message}")
+//            }
+//        })
 
-            }
-        })
 
         return usersResponse!!
-
     }
 }
