0cfa3864b2d30ba4c8915685dfe604f93931012f
sandeepkumar9248 <sandeep.k@peopelinkvc.com>
2022-06-08 16:18:32 +0530

    InApi sdk creation initialise function added

--- mylibrary/src/main/java/com/sampleunittest/mylibrary/InAppPeer.java
+++ mylibrary/src/main/java/com/sampleunittest/mylibrary/InAppPeer.java
@@ -240,20 +240,20 @@ public class InAppPeer {
 
         //CAN INITIALIZE SEPARATE
         mPeerConnectionFactory = createPeerConnectionFactory(context, mRootEglBase);
-        Logging.enableLogToDebugOutput(Logging.Severity.LS_VERBOSE);
-        mSurfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", mRootEglBase.getEglBaseContext());
-        VideoSource videoSource = mPeerConnectionFactory.createVideoSource(false);
-        mVideoCapturer = createVideoCapturer(context);
-        mVideoCapturer.initialize(mSurfaceTextureHelper, context, videoSource.getCapturerObserver());
-        mVideoTrack = mPeerConnectionFactory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
-        mVideoTrack.setEnabled(true);
-        mVideoTrack.addSink(mLocalSurfaceView);
-        AudioSource audioSource = mPeerConnectionFactory.createAudioSource(new MediaConstraints());
-        mAudioTrack = mPeerConnectionFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
-        mAudioTrack.setEnabled(true);
-
-        //TO START LOCAL CAPTURE
-        mVideoCapturer.startCapture(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT, VIDEO_FPS);
+//        Logging.enableLogToDebugOutput(Logging.Severity.LS_VERBOSE);
+//        mSurfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", mRootEglBase.getEglBaseContext());
+//        VideoSource videoSource = mPeerConnectionFactory.createVideoSource(false);
+//        mVideoCapturer = createVideoCapturer(context);
+//        mVideoCapturer.initialize(mSurfaceTextureHelper, context, videoSource.getCapturerObserver());
+//        mVideoTrack = mPeerConnectionFactory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
+//        mVideoTrack.setEnabled(true);
+//        mVideoTrack.addSink(mLocalSurfaceView);
+//        AudioSource audioSource = mPeerConnectionFactory.createAudioSource(new MediaConstraints());
+//        mAudioTrack = mPeerConnectionFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
+//        mAudioTrack.setEnabled(true);
+//
+//        //TO START LOCAL CAPTURE
+//        mVideoCapturer.startCapture(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT, VIDEO_FPS);
 
     }
 
