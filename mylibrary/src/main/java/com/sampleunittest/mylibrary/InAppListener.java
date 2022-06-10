78a47e081b5d4a6b5f220b06eaf05837c7655ff8
sandeepkumar9248 <sandeep.k@peopelinkvc.com>
2022-06-10 16:06:28 +0530

    InApi sdk creation initialise function added

--- mylibrary/src/main/java/com/sampleunittest/mylibrary/InAppListener.java
+++ mylibrary/src/main/java/com/sampleunittest/mylibrary/InAppListener.java
@@ -2,6 +2,7 @@ package com.sampleunittest.mylibrary;
 
 public interface InAppListener {
     void offerReceived(String remoteId);
+    void answerReceived(String message);
     void onFinished();
     void remoteUserDisconnected();
 }
